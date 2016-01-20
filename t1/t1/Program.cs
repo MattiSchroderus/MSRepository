da1c97099000c56a7042e45e11181ea6b891fca2
Matti Schroderus <mattischroderus37@gmail.com>
2016-01-20 19:00:30 +0200

    Kiuas.1

--- t1/t1/Program.cs
+++ t1/t1/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace t1
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
+            Kiuas harvia = new Kiuas();
+            harvia.Temp = 25;
+            harvia.Kuumaa();
+            harvia.Tarkistus();
+
+        }
+
+        class Kiuas
+        {
+            public int Temp { get; set; }
+
+            public void Kuumaa()
+            {
+                Temp += 3;
+            }
+            public void Kylmaa()
+            {
+                Temp -= 3;
+            }
+            public void Tarkistus()
+            {
+                Console.WriteLine("  Lampotila on " + Temp + " astetta." );
+            }
         }
     }
 }
