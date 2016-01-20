8fa32ffd8de1c24da1f4f8d9a46827b355c51ff8
Matti Schroderus <mattischroderus37@gmail.com>
2016-01-20 18:41:18 +0200

    gjd

--- githubtesti123/githubtesti123/Program.cs
+++ githubtesti123/githubtesti123/Program.cs
@@ -2,48 +2,10 @@
 
 namespace githubtesti123
 {
-    class Car
+    struct point
     {
-        // properties
-        public string Model { get; set; }
-        public string Color { get; set; }
-        public double Engine { get; set; }
-        public int Speed { get; set; }
-        public bool FuzzyDices { get; set; }
-        public int DoorCount { get; set; }
-
-        // don't create any constructor, so default one will be used
-
-        // method to give more speed
-        public void Accelerate()
-        {
-            Speed += 5;
-        }
-
-        // method to slow down
-        public void Brake()
-        {
-            Speed -= 5;
-        }
-
-        // method to display car data
-        public void PrintData()
-        {
-            Console.WriteLine("Car data : ");
-            Console.WriteLine("- model : " + Model);
-            Console.WriteLine("- color : " + Color);
-            Console.WriteLine("- engine : " + Engine);
-            Console.WriteLine("- speed : " + Speed);
-            Console.WriteLine("- fuzzy dices : " + FuzzyDices);
-            Console.WriteLine("- door count : " + DoorCount);
-        }
-
-        // destructor
-        ~Car()
-        {
-            Console.WriteLine("Car object destroyed.");
-        }
-
+        public int x;
+        public int y;
     }
 
     class Program
@@ -51,22 +13,21 @@ namespace githubtesti123
 
         static void Main(string[] args)
         {
-            // create a one car instance
-            Car datsun = new Car();
-            datsun.Model = "Datsun 100A";
-            datsun.Color = "red";
-            datsun.Engine = 1.0;
-            datsun.Speed = 0;
-            datsun.FuzzyDices = true;
-            datsun.DoorCount = 2;
-            // display car data
-            datsun.PrintData();
+            point coordinates = new point();
+            coordinates.x = 100;
+            coordinates.y = 50;
+
+            Modifypoint(coordinates);
+
+            Console.WriteLine("Coordinates: " + coordinates.x + "; " + coordinates.y);
             Console.ReadLine();
-            // accelerate, change car's speed value
-            datsun.Accelerate();
-            // display car data again, notice changed speed value!
-            datsun.PrintData();
-            Console.ReadLine();
+         
+        }
+        static void Modifypoint(point p)
+        {
+            p.x += 500;
+            p.y += 1500;
         }
     }
+    
 }
